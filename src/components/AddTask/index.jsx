7fd8dbfe769b157508338b438d4580903f39211f
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-04-19 15:12:26 +0530

    Add Task Components

--- src/components/AddTask/index.jsx
+++ src/components/AddTask/index.jsx
@@ -1,13 +1,17 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button, Group } from '@mantine/core';
+import { Modal, Button, Group, TextInput, Flex } from '@mantine/core';
 
 const AddTask = () => {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
-      <Modal opened={opened} onClose={close} title="Add Task">
-        {/* Modal content */}
+      <Modal opened={true} onClose={close} withCloseButton={false}>
+        <TextInput placeholder="Task Name" label="Add Task" />
+        <Flex gap="xs" justify="center" align="center" direction="row">
+          <Button>Cancel</Button>
+          <Button>Submit</Button>
+        </Flex>
       </Modal>
 
       <Group position="center">
