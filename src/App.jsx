266e4424efbc9fd9ac3f42d352ffabd6a6d0aa71
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-04-19 15:32:22 +0530

    Mock Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 import AddTask from './components/AddTask';
 import Layout from './layouts';
 
 function App() {
-  return <Layout addTask={AddTask}>Children</Layout>;
+  const [tasks, setTasks] = useState(['Task 1', 'Task 2']);
+
+  return (
+    <Layout addTask={AddTask}>
+      {tasks.map((task) => (
+        <p key={task}>{task}</p>
+      ))}
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/AddTask/index.jsx
+++ src/components/AddTask/index.jsx
@@ -1,5 +1,5 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button, Group, TextInput, Flex } from '@mantine/core';
+import { Modal, Button, Group, TextInput, Flex, Space } from '@mantine/core';
 
 const AddTask = () => {
   const [opened, { open, close }] = useDisclosure(false);
@@ -8,6 +8,7 @@ const AddTask = () => {
     <>
       <Modal opened={opened} onClose={close} withCloseButton={false}>
         <TextInput placeholder="Task Name" label="Add Task" />
+        <Space h="md" />
         <Flex gap="xs" justify="center" align="center" direction="row">
           <Button>Cancel</Button>
           <Button>Submit</Button>
